f1f49401e83a6dff2b9f81c6c0fb3cc9ed1827a9
sudo-001 <carick.atezong@facsciences-uy1.cm>
2022-08-08 05:29:47 +0100

    Ajout du loader

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ const temperature = document.querySelector(".temperature > span");
 const feels_like_temp = document.querySelector(".temperature > div > span");
 const description = document.querySelector(".description");
 const hotDeg = document.querySelector(".temp");
-
+const loaderWrapper = document.querySelector(".loader-wrapper");
+const loader = document.querySelector(".loader");
 var today = new Date();
 
 // Openweathermap API KEY
@@ -24,24 +25,35 @@ form.addEventListener("submit", async function (e) {
 
 });
 
+let isLoading = false;
 
 function getCoordonates(name) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=10&appid=${apiKey}`)
         .then(res => res.json())
         .then(data => {
+            isLoading = true;
+            loaderWrapper.style.display = "inherit";
+            loader.style.animation = "rotate infinite 2s linear";
             coordonates = data;
             console.log(coordonates);
             getWeatherData(coordonates[0].lat, coordonates[0].lon);
         })
-        .catch(err => console.log({ err }))
+        .catch(err => {
+            if (err.message.match(/Cannot read properties of undefined/)) {
+                loader.style.animation = "";
+                loaderWrapper.style.display = "none";
+                alert("Il se pourrais que cette ville n'existe pas ");
+            }
+            console.log({ err })
+        })
 }
 
 function getWeatherData(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=fr&units=metric`)
         .then(res => res.json())
         .then(data => {
-            currentDate = today.getFullYear() +"/"+(today.getMonth()+1)+"/"+today.getDate();
-            
+            currentDate = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate();
+
             console.log(data);
 
             nomVille.innerHTML = data.name + " , " + data.sys.country;
@@ -51,14 +63,24 @@ function getWeatherData(lat, lon) {
             feels_like_temp.innerHTML = data.main.feels_like + "°";
             let icon = data.weather[0].icon.replace('n', 'd');
             console.log(icon);
-            
+
             image.setAttribute("src", ` http://openweathermap.org/img/wn/${icon}.png`);
-            if (data.main.temp <= 25 ) {
-                hotDeg.style.backgroundColor = `rgba(255, 255, 255, 0.${(Math.floor(data.main.temp))})`;   
+            if (data.main.temp <= 25) {
+                hotDeg.style.backgroundColor = `rgba(255, 255, 255, 0.${(Math.floor(data.main.temp))})`;
             } else {
                 hotDeg.style.backgroundColor = `rgba(${(Math.floor(data.main.temp))}, 0,0, 0.4)`;
             }
-            
+
+            loader.style.animation = "";
+            loaderWrapper.style.display = "none";
+
         })
-        .catch(err => console.log({ err }))
+        .catch(err => {
+            if (err.message.match(/Cannot read properties of undefined/)) {
+                loader.style.animation = "";
+                loaderWrapper.style.display = "none";
+                alert("Il se pourrais que cette ville n'existe pas ");
+            }
+            console.log({ err });
+        })
 }
